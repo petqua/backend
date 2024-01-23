10ec2c42defd054e3bbef6254d5dde04d4d9184a
Combi153 <jcm04@naver.com>
2024-01-23 17:02:36 +0900

    build: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 }
 
 tasks.withType<KotlinCompile> {
