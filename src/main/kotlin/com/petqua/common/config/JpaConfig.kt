58395e1481f9b3aada74ebbdac92729e5c55b4b5
Combi153 <jcm04@naver.com>
2024-01-23 17:02:55 +0900

    feat: BaseEntity 추가

---
+++ src/main/kotlin/com/petqua/common/config/JpaConfig.kt
@@ -0,0 +1,9 @@
+package com.petqua.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig {
+}
