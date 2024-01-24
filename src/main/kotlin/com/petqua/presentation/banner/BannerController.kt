e4116e028bdbd8a937c88441813fcc4b6f812ddf
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-24 17:40:45 +0900

    feat: Banner 조회 API 추가

---
+++ src/main/kotlin/com/petqua/presentation/banner/BannerController.kt
@@ -0,0 +1,21 @@
+package com.petqua.presentation.banner
+
+import com.petqua.application.banner.BannerService
+import com.petqua.application.banner.FindBannerResult
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/banner")
+@RestController
+class BannerController(
+    private val bannerService: BannerService
+) {
+
+    @GetMapping
+    fun getBanners(): ResponseEntity<List<FindBannerResult>> {
+        val bannerList = bannerService.getBannerList()
+        return ResponseEntity.ok(bannerList)
+    }
+}
