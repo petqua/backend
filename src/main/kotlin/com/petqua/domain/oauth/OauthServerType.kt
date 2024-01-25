73b8aad8bbbe7602e4438a7d786cc79716ea39a2
hgo641 <hgo641@gmail.com>
2024-01-26 04:09:19 +0900

    feat: OauthServerType에 필드 추가

--- src/main/kotlin/com/petqua/domain/oauth/OauthServerType.kt
+++ src/main/kotlin/com/petqua/domain/oauth/OauthServerType.kt
@@ -1,15 +1,19 @@
 package com.petqua.domain.oauth
 
 import com.petqua.common.exception.oauth.OauthClientException
+import com.petqua.common.exception.oauth.OauthClientExceptionType.UNSUPPORTED_OAUTH_SERVER_TYPE
 import java.util.Locale.ENGLISH
 
-enum class OauthServerType {
-    KAKAO;
+enum class OauthServerType(
+    val number: Int
+) {
+    KAKAO(1)
+    ;
 
     companion object {
         fun from(name: String): OauthServerType {
             return enumValues<OauthServerType>().find { it.name == name.uppercase(ENGLISH) }
-                ?: throw OauthClientException()
+                ?: throw OauthClientException(UNSUPPORTED_OAUTH_SERVER_TYPE)
         }
     }
 }
