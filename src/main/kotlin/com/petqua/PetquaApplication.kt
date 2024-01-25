bad7459fa5435651b9abc1262a8651147ab8489f
hgo641 <hgo641@gmail.com>
2024-01-26 04:08:53 +0900

    chore: 어노테이션 순서 변경

--- src/main/kotlin/com/petqua/PetquaApplication.kt
+++ src/main/kotlin/com/petqua/PetquaApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
 @ConfigurationPropertiesScan
+@SpringBootApplication
 class PetquaApplication
 
 fun main(args: Array<String>) {
