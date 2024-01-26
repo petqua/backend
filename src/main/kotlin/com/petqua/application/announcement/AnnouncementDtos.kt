313a2b3d8953929ffc161c64c4a78fd636e186d8
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-26 21:11:18 +0900

    refactor: 불필요한 응답 필드 제거

--- src/main/kotlin/com/petqua/application/announcement/AnnouncementDtos.kt
+++ src/main/kotlin/com/petqua/application/announcement/AnnouncementDtos.kt
@@ -1,14 +1,11 @@
 package com.petqua.application.announcement
 
 import com.petqua.domain.announcement.Announcement
-import java.time.LocalDateTime
 
 data class AnnouncementResponse(
     val id: Long,
     val title: String,
     val linkUrl: String,
-    val createAt: LocalDateTime,
-    val updateAt: LocalDateTime,
 ) {
     companion object {
         fun from(announcement: Announcement): AnnouncementResponse {
@@ -16,8 +13,6 @@ data class AnnouncementResponse(
                 id = announcement.id,
                 title = announcement.title,
                 linkUrl = announcement.linkUrl,
-                createAt = announcement.createAt,
-                updateAt = announcement.updateAt,
             )
         }
     }
