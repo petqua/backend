9db679352e7bed821106c592a80cae23894ba3a7
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-26 15:07:51 +0900

    style: static import 제거

--- src/main/kotlin/com/petqua/domain/banner/Banner.kt
+++ src/main/kotlin/com/petqua/domain/banner/Banner.kt
@@ -1,7 +1,11 @@
 package com.petqua.domain.banner
 
 import com.petqua.common.domain.BaseEntity
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 
 @Entity
 class Banner(
