2ab7f301898103144aa46f452382ff458cecf37d
hgo641 <hgo641@gmail.com>
2024-01-29 08:43:03 +0900

    chore: 사용하지 않는 코드 삭제

--- src/main/kotlin/com/petqua/domain/oauth/OauthTokenInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.petqua.domain.oauth
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
-import com.fasterxml.jackson.databind.annotation.JsonNaming
-
-@JsonNaming(SnakeCaseStrategy::class)
-data class OauthTokenInfo (
-    val accessToken: String
-)
