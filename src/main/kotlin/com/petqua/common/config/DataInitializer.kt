474ee6d985febd885c4c004e589979d3f83d8da2
Combi153 <jcm04@naver.com>
2024-01-29 15:37:22 +0900

    fix: 테스트 profile 분리

--- src/main/kotlin/com/petqua/common/config/DataInitializer.kt
+++ src/main/kotlin/com/petqua/common/config/DataInitializer.kt
@@ -11,12 +11,14 @@ import com.petqua.domain.recommendation.ProductRecommendationRepository
 import com.petqua.domain.store.Store
 import com.petqua.domain.store.StoreRepository
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 
 @Component
+@Profile("local", "prod")
 class DataInitializer(
     private val announcementRepository: AnnouncementRepository,
     private val bannerRepository: BannerRepository,
