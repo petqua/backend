042708bab1c302c1b1e5aea0e4c358a99a74b578
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-29 16:26:20 +0900

    feat: Entity 존재 여부 확인 확장 함수 추가

--- src/main/kotlin/com/petqua/common/domain/Repository.kt
+++ src/main/kotlin/com/petqua/common/domain/Repository.kt
@@ -6,3 +6,9 @@ import org.springframework.data.repository.findByIdOrNull
 inline fun <reified T, ID> CrudRepository<T, ID>.findByIdOrThrow(
     id: ID, e: Exception = IllegalArgumentException("${T::class.java.name} entity 를 찾을 수 없습니다. id=$id")
 ): T = findByIdOrNull(id) ?: throw e
+
+
+inline fun <reified T, ID> CrudRepository<T, ID>.existByIdOrThrow(
+    id: ID,
+    e: Exception = IllegalArgumentException("${T::class.java.name} entity 를 찾을 수 없습니다. id=$id")
+): Unit = if (!existsById(id!!)) throw e else Unit
