bd5313afd4bc8d4eb2e7cf7e23613db8dd4f3b3c
Chanmin Ju(Hu chu) <jcm04@naver.com>
2024-01-29 14:02:04 +0900

    feature: cors 관련 설정을 추가한다 (#24)
    * feat: cors configuration 추가
    
    * refactor: CacheConfig 이름 및 패키지 변경

---
+++ src/main/kotlin/com/petqua/common/config/CorsConfig.kt
@@ -0,0 +1,30 @@
+package com.petqua.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.DELETE
+import org.springframework.http.HttpMethod.GET
+import org.springframework.http.HttpMethod.OPTIONS
+import org.springframework.http.HttpMethod.PATCH
+import org.springframework.http.HttpMethod.POST
+import org.springframework.http.HttpMethod.PUT
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "https://petqua.co.kr")
+            .allowedMethods(
+                OPTIONS.name(),
+                GET.name(),
+                POST.name(),
+                PUT.name(),
+                PATCH.name(),
+                DELETE.name(),
+            )
+            .allowCredentials(true)
+            .exposedHeaders("*")
+    }
+}
