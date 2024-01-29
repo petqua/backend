b6dd112af21a70259a1e3ff28fa86fff15a78471
hgo641 <hgo641@gmail.com>
2024-01-29 16:38:45 +0900

    refactor: AuthException 이름 변경 및 예외 타입 추가

--- src/main/kotlin/com/petqua/common/exception/auth/AuthException.kt
+++ src/main/kotlin/com/petqua/common/exception/auth/AuthException.kt
@@ -3,11 +3,11 @@ package com.petqua.common.exception.auth
 import com.petqua.common.exception.BaseException
 import com.petqua.common.exception.BaseExceptionType
 
-class AuthTokenException(
-    private val authTokenExceptionType: AuthTokenExceptionType
+class AuthException(
+    private val authExceptionType: AuthExceptionType
 ) : BaseException() {
 
     override fun exceptionType(): BaseExceptionType {
-        return authTokenExceptionType
+        return authExceptionType
     }
 }
