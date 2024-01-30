6fa783dd99903afb3bf3194f9caba21de31c970d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-30 15:19:17 +0900

    refactor: 중복된 dependency 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,15 +20,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    // kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // spring data jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // spring boot web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // jwt
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+
+    // annotation processor
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     // kotlin jdsl
@@ -37,8 +45,7 @@ dependencies {
 
     // spring boot cache
     implementation("org.springframework.boot:spring-boot-starter-cache")
-
-
+    
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
 
