c6e37a60637fac02f90093f5b382356eac00a81e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-01-30 15:28:14 +0900

    feat: 회원 존재 유무 검증 로직 추가

--- src/main/kotlin/com/petqua/application/cart/CartProductService.kt
+++ src/main/kotlin/com/petqua/application/cart/CartProductService.kt
@@ -3,7 +3,10 @@ package com.petqua.application.cart
 import com.petqua.application.cart.dto.SaveCartProductCommand
 import com.petqua.common.domain.existByIdOrThrow
 import com.petqua.domain.cart.CartProductRepository
+import com.petqua.domain.member.MemberRepository
 import com.petqua.domain.product.ProductRepository
+import com.petqua.exception.member.MemberException
+import com.petqua.exception.member.MemberExceptionType.NOT_FOUND_MEMBER
 import com.petqua.exception.product.ProductException
 import com.petqua.exception.product.ProductExceptionType.NOT_FOUND_PRODUCT
 import org.springframework.stereotype.Service
@@ -14,10 +17,11 @@ import org.springframework.transaction.annotation.Transactional
 class CartProductService(
     private val cartProductRepository: CartProductRepository,
     private val productRepository: ProductRepository,
+    private val memberRepository: MemberRepository,
 ) {
 
     fun save(command: SaveCartProductCommand): Long {
-//      TODO  memberRepository.existByIdOrThrow(command.memberId, MemberException(NOT_FOUND_MEMBER))
+        memberRepository.existByIdOrThrow(command.memberId, MemberException(NOT_FOUND_MEMBER))
         productRepository.existByIdOrThrow(command.productId, ProductException(NOT_FOUND_PRODUCT))
         val savedCartProduct = cartProductRepository.save(command.toCartProduct())
         return savedCartProduct.id
