5a8b1f521955c733a3c6f1b1a937dbee1d0c8d40
hgo641 <hgo641@gmail.com>
2024-01-30 15:54:05 +0900

    refactor: 쿠키 설정 추가

--- src/main/kotlin/com/petqua/presentation/auth/OauthController.kt
+++ src/main/kotlin/com/petqua/presentation/auth/OauthController.kt
@@ -1,10 +1,10 @@
 package com.petqua.presentation.auth
 
-import com.petqua.application.auth.AuthResponse
 import com.petqua.application.auth.OauthService
 import com.petqua.domain.auth.oauth.OauthServerType
 import org.springframework.http.HttpHeaders.SET_COOKIE
 import org.springframework.http.HttpStatus.FOUND
+import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -33,10 +33,18 @@ class OauthController(
         @PathVariable oauthServerType: OauthServerType,
         @RequestParam("code") code: String,
     ): ResponseEntity<AuthResponse> {
-        val oauthResponse = oauthService.login(oauthServerType, code)
+        val authTokenInfo = oauthService.login(oauthServerType, code)
+        val refreshTokenCookie = ResponseCookie.from("refresh-token", authTokenInfo.refreshToken)
+            .sameSite("None")
+            .secure(true)
+            .httpOnly(true)
+            .build()
+        val authResponse = AuthResponse(
+            accessToken = authTokenInfo.accessToken
+        )
         return ResponseEntity
             .ok()
-            .header(SET_COOKIE, oauthResponse.refreshToken)
-            .body(oauthResponse)
+            .header(SET_COOKIE, refreshTokenCookie.toString())
+            .body(authResponse)
     }
 }
