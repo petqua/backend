7977d1376882a02f9413e653d42d22075abb2776
hgo641 <hgo641@gmail.com>
2024-01-31 15:48:21 +0900

    test: 로그인 연장 테스트 추가

--- src/test/kotlin/com/petqua/application/auth/AuthServiceTest.kt
+++ src/test/kotlin/com/petqua/application/auth/AuthServiceTest.kt
@@ -1,33 +1,40 @@
 package com.petqua.application.auth
 
 import com.petqua.domain.auth.oauth.OauthServerType.KAKAO
-import com.petqua.domain.auth.token.JwtProvider
+import com.petqua.domain.auth.token.AuthTokenProvider
+import com.petqua.domain.auth.token.RefreshToken
 import com.petqua.domain.auth.token.RefreshTokenRepository
+import com.petqua.domain.member.MemberRepository
+import com.petqua.test.DataCleaner
 import com.petqua.test.config.OauthTestConfig
+import com.petqua.test.fixture.member
 import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE
 import org.springframework.context.annotation.Import
+import java.util.Date
 
 @SpringBootTest(webEnvironment = NONE)
 @Import(OauthTestConfig::class)
 class AuthServiceTest(
     private var authService: AuthService,
     private val refreshTokenRepository: RefreshTokenRepository,
-    private val jwtProvider: JwtProvider,
+    private val authTokenProvider: AuthTokenProvider,
+    private val memberRepository: MemberRepository,
+    private val dataCleaner: DataCleaner,
 ) : BehaviorSpec({
 
-    Given("소셜 로그인 테스트") {
+    Given("카카오 소셜 로그인을") {
 
-        When("OauthServerType과 accessCode를 가지고 로그인을 하면") {
+        When("요청하면") {
             val authTokenInfo = authService.login(KAKAO, "accessCode")
 
             Then("멤버의 인증 토큰을 발급한다") {
                 assertSoftly(authTokenInfo) {
-                    jwtProvider.isValidToken(accessToken) shouldBe true
-                    jwtProvider.isValidToken(refreshToken) shouldBe true
+                    authTokenProvider.isValidToken(accessToken) shouldBe true
+                    authTokenProvider.isValidToken(refreshToken) shouldBe true
                 }
             }
 
@@ -35,5 +42,36 @@ class AuthServiceTest(
                 refreshTokenRepository.existsByToken(authTokenInfo.refreshToken) shouldBe true
             }
         }
+    }
+
+    Given("로그인 연장을") {
+        val member = memberRepository.save(member())
+        val expiredAccessToken = authTokenProvider.createAuthToken(member, Date(0)).accessToken
+        val refreshToken = authTokenProvider.createAuthToken(member, Date()).refreshToken
+        println("exp : " + expiredAccessToken)
+        println("ref : " + refreshToken)
+        refreshTokenRepository.save(
+            RefreshToken(
+                memberId = member.id,
+                token = refreshToken
+            )
+        )
+
+        When("요청하면") {
+            val authTokenInfo = authService.extendLogin(expiredAccessToken, refreshToken)
+
+            Then("멤버의 인증 토큰을 발급한다") {
+                authTokenProvider.isValidToken(authTokenInfo.accessToken) shouldBe true
+                authTokenProvider.isValidToken(authTokenInfo.refreshToken) shouldBe true
+            }
+
+            Then("발급한 refreshToken을 저장한다") {
+                refreshTokenRepository.existsByToken(authTokenInfo.refreshToken) shouldBe true
+            }
+        }
+    }
+
+    afterContainer {
+        dataCleaner.clean()
     }
 })
