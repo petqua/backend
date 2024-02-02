a0c75bf785125587b9e57790417f52536b2913cd
hgo641 <hgo641@gmail.com>
2024-02-03 02:24:50 +0900

    chore: Long 타입 명시

--- src/test/kotlin/com/petqua/test/fixture/WishProductFixtures.kt
+++ src/test/kotlin/com/petqua/test/fixture/WishProductFixtures.kt
@@ -3,9 +3,9 @@ package com.petqua.test.fixture
 import com.petqua.domain.product.WishProduct
 
 fun wishProduct(
-    id: Long = 0,
-    productId: Long = 1,
-    memberId: Long = 1,
+    id: Long = 0L,
+    productId: Long = 1L,
+    memberId: Long = 1L,
 ): WishProduct {
     return WishProduct(
         id = id,
