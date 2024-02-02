205d6424db8604463765258ad6bb13728e4651df
hgo641 <hgo641@gmail.com>
2024-02-03 02:52:03 +0900

    refactor: AsyncConfig 생성

--- src/main/kotlin/com/petqua/PetquaApplication.kt
+++ src/main/kotlin/com/petqua/PetquaApplication.kt
@@ -3,11 +3,9 @@ package com.petqua
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableAsync
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-@EnableAsync
 class PetquaApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/petqua/common/config/AsyncConfig.kt
@@ -0,0 +1,9 @@
+package com.petqua.common.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+}
