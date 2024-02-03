63a88aa5ec532d6b0a18c38228782624a68f6c23
hgo641 <hgo641@gmail.com>
2024-02-03 23:24:29 +0900

    feat: 멤버별 찜 목록 개수를 세는 기능 추가

--- src/main/kotlin/com/petqua/domain/product/WishProductRepository.kt
+++ src/main/kotlin/com/petqua/domain/product/WishProductRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface WishProductRepository : JpaRepository<WishProduct, Long>, WishProductCustomRepository {
 
     fun existsByProductIdAndMemberId(productId: Long, memberId: Long): Boolean
+
+    fun countByMemberId(memberId: Long): Int
 }
