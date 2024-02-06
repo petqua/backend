513acf033682dc1fe74ea765ec77e7abc13ef6f7
Combi153 <jcm04@naver.com>
2024-02-06 12:56:50 +0900

    refactor: AuthController Hidden 처리

--- src/main/kotlin/com/petqua/presentation/auth/AuthController.kt
+++ src/main/kotlin/com/petqua/presentation/auth/AuthController.kt
@@ -6,9 +6,6 @@ import com.petqua.domain.auth.Auth
 import com.petqua.domain.auth.oauth.OauthServerType
 import com.petqua.domain.auth.token.AuthToken
 import io.swagger.v3.oas.annotations.Hidden
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Schema
-import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpHeaders.SET_COOKIE
 import org.springframework.http.HttpStatus.FOUND
@@ -20,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@Tag(name = "Auth", description = "인증 관련 API 명세")
+@Tag(name = "Auth", description = "인증 관련 API 명세(Swagger에서 테스트할 수 없어 숨김 처리 되었습니다.)")
 @RequestMapping("/auth")
 @RestController
 class AuthController(
@@ -28,11 +25,8 @@ class AuthController(
 ) {
 
     @Hidden
-    @Operation(summary = "리다이렉트 요청 API", description = "Oauth 로그인 페이지로 리다이렉트합니다")
-    @ApiResponse(responseCode = "302", description = "리다이렉트 성공")
     @GetMapping("/{oauthServerType}")
     fun redirectToAuthCodeRequestUrl(
-        @Schema(description = "Oauth 서버", example = "KAKAO")
         @PathVariable oauthServerType: OauthServerType,
     ): ResponseEntity<Unit> {
         val redirectUri = authService.getAuthCodeRequestUrl(oauthServerType)
@@ -42,14 +36,9 @@ class AuthController(
     }
 
     @Hidden
-    @Operation(summary = "소셜 로그인 API", description = "소셜 로그인을 합니다")
-    @ApiResponse(responseCode = "200", description = "로그인 성공")
     @GetMapping("/login/{oauthServerType}")
     fun login(
-        @Schema(description = "Oauth 서버", example = "KAKAO")
         @PathVariable oauthServerType: OauthServerType,
-
-        @Schema(description = "auth code")
         @RequestParam("code") code: String,
     ): ResponseEntity<AuthResponse> {
         val authTokenInfo = authService.login(oauthServerType, code)
@@ -63,8 +52,7 @@ class AuthController(
             .body(authResponse)
     }
 
-    @Operation(summary = "로그인 유지 API", description = "refresh token으로 access token을 재발급 받습니다")
-    @ApiResponse(responseCode = "200", description = "재발급 성공")
+    @Hidden
     @GetMapping("/token")
     fun extendLogin(
         @Auth authToken: AuthToken,
--- src/main/kotlin/com/petqua/presentation/auth/AuthDtos.kt
+++ src/main/kotlin/com/petqua/presentation/auth/AuthDtos.kt
@@ -1,8 +1,5 @@
 package com.petqua.presentation.auth
 
-import io.swagger.v3.oas.annotations.media.Schema
-
 data class AuthResponse(
-    @Schema(description = "access token", example = "xxx.yyy.zzz")
     val accessToken: String,
 )
