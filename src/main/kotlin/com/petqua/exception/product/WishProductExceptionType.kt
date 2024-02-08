f246f04dc12de63d2f9dfbf75f82af3441fc6e48
hgo641 <hgo641@gmail.com>
2024-02-08 11:18:48 +0900

    refactor: 사용 하지 않는 예외 삭제

--- src/main/kotlin/com/petqua/exception/product/WishProductExceptionType.kt
+++ src/main/kotlin/com/petqua/exception/product/WishProductExceptionType.kt
@@ -12,9 +12,7 @@ enum class WishProductExceptionType(
     private val errorMessage: String,
 ) : BaseExceptionType {
 
-    NOT_FOUND_WISH_PRODUCT(NOT_FOUND, "WP01", "존재하지 않는 찜 상품입니다."),
     FORBIDDEN_WISH_PRODUCT(FORBIDDEN, "WP10", "해당 찜 상품에 대한 권한이 없습니다."),
-    ALREADY_EXIST_WISH_PRODUCT(BAD_REQUEST, "WP11", "이미 존재하는 찜 상품입니다."),
     ;
 
     override fun httpStatus(): HttpStatus {
