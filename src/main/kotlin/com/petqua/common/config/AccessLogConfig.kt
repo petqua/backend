32c931ea3ab8446469a3307f64afcef41b928bf4
hgo641 <hgo641@gmail.com>
2024-02-14 22:08:42 +0900

    refactor: 필요없는 resourceLoader코드 제거

--- src/main/kotlin/com/petqua/common/config/AccessLogConfig.kt
+++ src/main/kotlin/com/petqua/common/config/AccessLogConfig.kt
@@ -1,33 +1,21 @@
 package com.petqua.common.config
 
 import ch.qos.logback.access.tomcat.LogbackValve
-import org.apache.catalina.Context
-import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
-import org.springframework.boot.web.server.WebServerFactory
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.io.ResourceLoader
 
 
 @Configuration
-class AccessLogConfig(
-    private val resourceLoader: ResourceLoader
-) {
+class AccessLogConfig {
 
     @Bean
-    fun webServerFactoryCustomizer(): WebServerFactoryCustomizer<*> {
-        return WebServerFactoryCustomizer { container: WebServerFactory? ->
-            if (container is TomcatServletWebServerFactory) {
-                container.addContextCustomizers(TomcatContextCustomizer { context: Context ->
-                    val valve = LogbackValve()
-                    valve.filename = resourceLoader
-                        .getResource(ResourceLoader.CLASSPATH_URL_PREFIX + "logback-access.xml")
-                        .filename
-                    context.pipeline.addValve(valve)
-                })
-            }
+    fun webServerFactoryCustomizer(): WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+        return WebServerFactoryCustomizer<TomcatServletWebServerFactory> { factory ->
+            val logbackValve = LogbackValve()
+            logbackValve.filename = "logback-access.xml"
+            factory.addContextValves(logbackValve)
         }
     }
 }
