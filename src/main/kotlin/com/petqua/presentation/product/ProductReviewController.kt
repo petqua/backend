338de8f93a78d6613dc79c662b46aff2ef5bdd06
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-02-16 14:51:02 +0900

    docs: Swagger 인증 정보 추가

--- src/main/kotlin/com/petqua/presentation/product/ProductReviewController.kt
+++ src/main/kotlin/com/petqua/presentation/product/ProductReviewController.kt
@@ -3,12 +3,14 @@ package com.petqua.presentation.product
 import com.petqua.application.product.dto.ProductReviewStatisticsResponse
 import com.petqua.application.product.dto.ProductReviewsResponse
 import com.petqua.application.product.review.ProductReviewService
+import com.petqua.common.config.ACCESS_TOKEN_SECURITY_SCHEME_KEY
 import com.petqua.domain.auth.Auth
 import com.petqua.domain.auth.LoginMember
 import com.petqua.presentation.product.dto.ReadAllProductReviewsRequest
 import com.petqua.presentation.product.dto.UpdateReviewRecommendationRequest
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -52,6 +54,7 @@ class ProductReviewController(
 
     @Operation(summary = "상품 후기 추천 토글 API", description = "상품 후기의 추천 여부를 토글합니다")
     @ApiResponse(responseCode = "204", description = "상품 후기 추천 토글 성공")
+    @SecurityRequirement(name = ACCESS_TOKEN_SECURITY_SCHEME_KEY)
     @PostMapping("/product-reviews/recommendation")
     fun updateRecommendation(
         @Auth loginMember: LoginMember,
