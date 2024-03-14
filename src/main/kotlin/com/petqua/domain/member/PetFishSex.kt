b7a53f5502093f1d6c9fc499012053eea79c94d8
Combi153 <jcm04@naver.com>
2024-03-14 18:58:50 +0900

    feat: PetFishSex, TankSize 정적 팩터리 메서드 추가

--- src/main/kotlin/com/petqua/domain/member/PetFishSex.kt
+++ src/main/kotlin/com/petqua/domain/member/PetFishSex.kt
@@ -1,5 +1,9 @@
 package com.petqua.domain.member
 
+import com.petqua.exception.member.MemberException
+import com.petqua.exception.member.MemberExceptionType.INVALID_MEMBER_FISH_SEX
+import java.util.Locale.ENGLISH
+
 enum class PetFishSex(
     val description: String,
 ) {
@@ -9,4 +13,11 @@ enum class PetFishSex(
     HERMAPHRODITE("자웅동체"),
     NONE("성별을 모르겠어요")
     ;
+
+    companion object {
+        fun from(name: String): PetFishSex {
+            return enumValues<PetFishSex>().find { it.name == name.uppercase(ENGLISH) }
+                ?: throw MemberException(INVALID_MEMBER_FISH_SEX)
+        }
+    }
 }
