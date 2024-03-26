c75a29316ccb8a9d17c7de15cf2adf3775c0ef5f
hgo641 <hgo641@gmail.com>
2024-03-27 04:26:42 +0900

    refactor: 최근 ProductSnapshot들만 조회하게 변경

--- src/main/kotlin/com/petqua/domain/product/ProductSnapshotRepository.kt
+++ src/main/kotlin/com/petqua/domain/product/ProductSnapshotRepository.kt
@@ -1,10 +1,12 @@
 package com.petqua.domain.product
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface ProductSnapshotRepository : JpaRepository<ProductSnapshot, Long> {
 
     fun findByProductIdOrderByIdDesc(productId: Long): ProductSnapshot?
 
+    @Query("SELECT ps FROM ProductSnapshot ps WHERE ps.id IN (SELECT MAX(ps2.id) FROM ProductSnapshot ps2 WHERE ps2.productId IN :productIds GROUP BY ps2.productId)")
     fun findAllByProductIdIn(productIds: List<Long>): List<ProductSnapshot>
 }
