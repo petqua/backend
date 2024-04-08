9ea90698421443397120675adc880369c2e9cf1f
Combi153 <jcm04@naver.com>
2024-04-08 22:26:24 +0900

    feat: s3 config 추가

---
+++ src/main/kotlin/com/petqua/common/config/S3Config.kt
@@ -0,0 +1,20 @@
+package com.petqua.common.config
+
+import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper
+import com.amazonaws.regions.Regions.AP_NORTHEAST_2
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class S3Config {
+
+    @Bean
+    fun amazonS3(): AmazonS3 {
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(AP_NORTHEAST_2)
+            .withCredentials(EC2ContainerCredentialsProviderWrapper())
+            .build()
+    }
+}
