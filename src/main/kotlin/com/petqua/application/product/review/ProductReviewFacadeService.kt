94b0962495b9ee07d0bba92f999536805c18e0dd
Combi153 <jcm04@naver.com>
2024-04-21 17:36:47 +0900

    feat: ProductReviewController 후기 작성 api 추가

--- src/main/kotlin/com/petqua/application/product/review/ProductReviewFacadeService.kt
+++ src/main/kotlin/com/petqua/application/product/review/ProductReviewFacadeService.kt
@@ -1,6 +1,10 @@
 package com.petqua.application.product.review
 
 import com.petqua.application.product.dto.ProductReviewCreateCommand
+import com.petqua.application.product.dto.ProductReviewReadQuery
+import com.petqua.application.product.dto.ProductReviewStatisticsResponse
+import com.petqua.application.product.dto.ProductReviewsResponse
+import com.petqua.application.product.dto.UpdateReviewRecommendationCommand
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,5 +17,17 @@ class ProductReviewFacadeService(
         val productReview = command.toProductReview()
         val reviewImageUrls = productReviewImageUploader.uploadAll(command.images)
         return productReviewService.create(productReview, reviewImageUrls)
+    }
+
+    fun readAll(query: ProductReviewReadQuery): ProductReviewsResponse {
+        return productReviewService.readAll(query)
+    }
+
+    fun readReviewCountStatistics(productId: Long): ProductReviewStatisticsResponse {
+        return productReviewService.readReviewCountStatistics(productId)
+    }
+
+    fun updateReviewRecommendation(command: UpdateReviewRecommendationCommand) {
+        productReviewService.updateReviewRecommendation(command)
     }
 }
