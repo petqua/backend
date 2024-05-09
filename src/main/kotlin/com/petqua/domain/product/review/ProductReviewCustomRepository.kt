5357fa51dcdc1d3cfd29258071ac1f0813b8be65
Combi153 <jcm04@naver.com>
2024-05-09 20:40:48 +0900

    feat: Repository 회원의 리뷰 내역 조회 기능 추가

--- src/main/kotlin/com/petqua/domain/product/review/ProductReviewCustomRepository.kt
+++ src/main/kotlin/com/petqua/domain/product/review/ProductReviewCustomRepository.kt
@@ -1,6 +1,7 @@
 package com.petqua.domain.product.review
 
 import com.petqua.common.domain.dto.CursorBasedPaging
+import com.petqua.domain.product.dto.MemberProductReviewResponse
 import com.petqua.domain.product.dto.ProductReviewReadCondition
 import com.petqua.domain.product.dto.ProductReviewScoreWithCount
 import com.petqua.domain.product.dto.ProductReviewWithMemberResponse
@@ -9,8 +10,10 @@ interface ProductReviewCustomRepository {
 
     fun findAllByCondition(
         condition: ProductReviewReadCondition,
-        paging: CursorBasedPaging
+        paging: CursorBasedPaging,
     ): List<ProductReviewWithMemberResponse>
 
     fun findReviewScoresWithCount(productId: Long): List<ProductReviewScoreWithCount>
+
+    fun findMemberProductReviewBy(memberId: Long, paging: CursorBasedPaging): List<MemberProductReviewResponse>
 }
--- src/test/kotlin/com/petqua/domain/product/review/ProductReviewCustomRepositoryImplTest.kt
+++ src/test/kotlin/com/petqua/domain/product/review/ProductReviewCustomRepositoryImplTest.kt
@@ -1,13 +1,21 @@
 package com.petqua.domain.product.review
 
 import com.petqua.common.domain.dto.CursorBasedPaging
+import com.petqua.domain.delivery.DeliveryMethod.SAFETY
 import com.petqua.domain.member.MemberRepository
+import com.petqua.domain.order.OrderNumber
+import com.petqua.domain.order.OrderPayment
+import com.petqua.domain.order.OrderPaymentRepository
+import com.petqua.domain.order.OrderRepository
+import com.petqua.domain.order.OrderStatus.PURCHASE_CONFIRMED
 import com.petqua.domain.product.ProductRepository
 import com.petqua.domain.product.dto.ProductReviewReadCondition
+import com.petqua.domain.product.option.Sex.FEMALE
 import com.petqua.domain.product.review.ProductReviewSorter.RECOMMEND_DESC
 import com.petqua.domain.store.StoreRepository
 import com.petqua.test.DataCleaner
 import com.petqua.test.fixture.member
+import com.petqua.test.fixture.order
 import com.petqua.test.fixture.product
 import com.petqua.test.fixture.productReview
 import com.petqua.test.fixture.store
@@ -18,6 +26,7 @@ import io.kotest.matchers.collections.shouldBeSortedWith
 import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 import java.math.BigDecimal
+import java.math.BigDecimal.ONE
 
 @SpringBootTest
 class ProductReviewCustomRepositoryImplTest(
@@ -25,6 +34,8 @@ class ProductReviewCustomRepositoryImplTest(
     private val productRepository: ProductRepository,
     private val storeRepository: StoreRepository,
     private val productReviewRepository: ProductReviewRepository,
+    private val orderRepository: OrderRepository,
+    private val orderPaymentRepository: OrderPaymentRepository,
     private val dataCleaner: DataCleaner,
 ) : BehaviorSpec({
 
@@ -224,6 +235,136 @@ class ProductReviewCustomRepositoryImplTest(
                     find { it.score == 5 }?.count shouldBe 3
                     find { it.score == 2 }?.count shouldBe 2
                 }
+            }
+        }
+    }
+
+    Given("사용자가 리뷰를 작성한 후 자신의 리뷰 내역을 조회할 때") {
+        val member = memberRepository.save(member(nickname = "쿠아"))
+        val store = storeRepository.save(store(name = "펫쿠아"))
+        val productA = productRepository.save(
+            product(
+                name = "상품A",
+                storeId = store.id,
+                discountPrice = BigDecimal.ZERO,
+                reviewCount = 0,
+                reviewTotalScore = 0
+            )
+        )
+        val productB = productRepository.save(
+            product(
+                name = "상품B",
+                storeId = store.id,
+                discountPrice = BigDecimal.ZERO,
+                reviewCount = 0,
+                reviewTotalScore = 0
+            )
+        )
+        val orderA = orderRepository.save(
+            order(
+                orderNumber = OrderNumber.from("202402211607020ORDERNUMBER"),
+                memberId = member.id,
+                storeId = store.id,
+                storeName = store.name,
+                quantity = 1,
+                totalAmount = ONE,
+                productId = productA.id,
+                productName = productA.name,
+                thumbnailUrl = productA.thumbnailUrl,
+                deliveryMethod = SAFETY,
+                sex = FEMALE,
+            )
+        )
+        val orderB = orderRepository.save(
+            order(
+                orderNumber = OrderNumber.from("202402211607021ORDERNUMBER"),
+                memberId = member.id,
+                storeId = store.id,
+                storeName = store.name,
+                quantity = 1,
+                totalAmount = ONE,
+                productId = productB.id,
+                productName = productB.name,
+                thumbnailUrl = productB.thumbnailUrl,
+                deliveryMethod = SAFETY,
+                sex = FEMALE,
+            )
+        )
+        orderPaymentRepository.saveAll(
+            listOf(
+                OrderPayment(
+                    orderId = orderA.id,
+                    status = PURCHASE_CONFIRMED
+                ),
+                OrderPayment(
+                    orderId = orderB.id,
+                    status = PURCHASE_CONFIRMED
+                )
+            )
+        )
+
+        val productReviewA = productReviewRepository.save(
+            productReview(
+                productId = productA.id,
+                reviewerId = member.id,
+                score = 5,
+                recommendCount = 1,
+                hasPhotos = false,
+                content = "상품A 정말 좋아요!"
+            )
+        )
+        val productReviewB = productReviewRepository.save(
+            productReview(
+                productId = productB.id,
+                reviewerId = member.id,
+                score = 5,
+                recommendCount = 1,
+                hasPhotos = false,
+                content = "상품B 정말 좋아요!"
+            )
+        )
+
+        When("자신의 리뷰 내역을 조회하면") {
+            val reviews = productReviewRepository.findMemberProductReviewBy(member.id, CursorBasedPaging())
+
+            Then("리뷰가 최신순으로 반환된다") {
+                reviews.size shouldBe 2
+
+                val memberProductReviewB = reviews[0]
+                memberProductReviewB.reviewId shouldBe productReviewB.id
+                memberProductReviewB.memberId shouldBe productReviewB.memberId
+                memberProductReviewB.createdAt shouldBe orderB.createdAt
+                memberProductReviewB.orderStatus shouldBe PURCHASE_CONFIRMED
+                memberProductReviewB.storeId shouldBe orderB.orderProduct.storeId
+                memberProductReviewB.storeId shouldBe orderB.orderProduct.storeId
+                memberProductReviewB.storeName shouldBe orderB.orderProduct.storeName
+                memberProductReviewB.productId shouldBe orderB.orderProduct.productId
+                memberProductReviewB.productName shouldBe orderB.orderProduct.productName
+                memberProductReviewB.productThumbnailUrl shouldBe orderB.orderProduct.thumbnailUrl
+                memberProductReviewB.quantity shouldBe orderB.orderProduct.quantity
+                memberProductReviewB.sex shouldBe orderB.orderProduct.sex
+                memberProductReviewB.deliveryMethod shouldBe orderB.orderProduct.deliveryMethod
+                memberProductReviewB.score shouldBe productReviewB.score
+                memberProductReviewB.content.value shouldBe productReviewB.content.value
+                memberProductReviewB.recommendCount shouldBe productReviewB.recommendCount
+
+                val memberProductReviewA = reviews[1]
+                memberProductReviewA.reviewId shouldBe productReviewA.id
+                memberProductReviewA.memberId shouldBe productReviewA.memberId
+                memberProductReviewA.createdAt shouldBe orderA.createdAt
+                memberProductReviewA.orderStatus shouldBe PURCHASE_CONFIRMED
+                memberProductReviewA.storeId shouldBe orderA.orderProduct.storeId
+                memberProductReviewA.storeId shouldBe orderA.orderProduct.storeId
+                memberProductReviewA.storeName shouldBe orderA.orderProduct.storeName
+                memberProductReviewA.productId shouldBe orderA.orderProduct.productId
+                memberProductReviewA.productName shouldBe orderA.orderProduct.productName
+                memberProductReviewA.productThumbnailUrl shouldBe orderA.orderProduct.thumbnailUrl
+                memberProductReviewA.quantity shouldBe orderA.orderProduct.quantity
+                memberProductReviewA.sex shouldBe orderA.orderProduct.sex
+                memberProductReviewA.deliveryMethod shouldBe orderA.orderProduct.deliveryMethod
+                memberProductReviewA.score shouldBe productReviewA.score
+                memberProductReviewA.content.value shouldBe productReviewA.content.value
+                memberProductReviewA.recommendCount shouldBe productReviewA.recommendCount
             }
         }
     }
